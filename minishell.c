6993526098c5d42dd9b570bbcd33025fcd703682
Isac Arnekvist <isacar@csc.kth.se>
2015-04-10 11:27:09 +0200

    Added polling of timing stat pipe

--- minishell.c
+++ minishell.c
@@ -3,13 +3,29 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <poll.h>
+#include "helpers.h"
 
 #define CMD_MAX_LEN 80
+#define TRUE 1
+#define FALSE 0
+#define READ_SIDE 0
+#define WRITE_SIDE 1
 
-void exec_foreground(char **); 
-void exec_background(char **); 
-char **args_tokenized(char *);
+/* TODO Bug: run 'cat', terminate with Ctrl-C. This will also terminate minishell. Sighandler? */
+
+/* This struct is used to send timing stats when process finished */
+typedef struct ptt {
+    pid_t           pid;
+    struct timeval  time;
+} proc_time_t ;
+
+/* A pipe to send proc_time_t through when processes finish */
+int proc_time_pipe[2];
+
+void interpret(char **, int);
 
 /* Without these, it says implicit declaration even though stdio.h is included? */
 int getline(char**, size_t*, FILE*);
@@ -19,15 +35,29 @@ int main() {
     char *input_string;
     char **args;
     size_t linecap;
-    int return_value;
+    int read_length;
+    /* int return_val; */
+    int is_background;
+    struct pollfd proc_time_pipe_poll;
+    proc_time_t proc_time;
     linecap = CMD_MAX_LEN;
+
+    /* Set up timing stats pipe */
+    if(-1 == pipe(proc_time_pipe)) {
+        perror("pipe");
+        exit(-1);
+    }
+
+    /* Set up polling of proc_time_pipe */
+    proc_time_pipe_poll.fd = proc_time_pipe[READ_SIDE];
+    proc_time_pipe_poll.events = POLLIN;
 
     while(1) {
         /* Read input */
         printf("> ");
-        return_value = getline(&input_string, &linecap, stdin);
-        input_string[return_value - 1] = '\0';
-        switch (return_value) {
+        read_length = getline(&input_string, &linecap, stdin);
+        input_string[read_length - 1] = '\0';
+        switch (read_length) {
             case -1:
                 /* If eof was entered or something went wrong, quietly quit
                  * (eof is bash behavior) */
@@ -36,84 +66,49 @@ int main() {
                 /* Empty line, new prompt */
                 break;
             default:
-
-                /* Check for '&', if '&' -> run in background, else foreground */
-                if(input_string[return_value-2] == '&') {
-                    /* Now ignore the '&' character */
-                    input_string[return_value-2] = '\0';
-
-                    /* Get array of input tokens (w/o '&') */
-                    args = args_tokenized(input_string);
-                    exec_background(args);
+                /* Handle '&' first. Might not be space separated, so easier to do here */
+                if('&' == input_string[read_length-2]) {
+                    /* '&' acknowledged, change to null */
+                    input_string[read_length-2] = '\0';
+                    is_background = TRUE;
                 } else {
-                    
-                    /* Get array of input tokens */
-                    args = args_tokenized(input_string);
-                    exec_foreground(args); 
-
+                    is_background = FALSE;
                 }
+
+                /* Get array of input tokens */
+                args = args_tokenized(input_string);
+                interpret(args, is_background); 
                 break;
+        }
+
+        /* Check if any processes finished */
+        while(poll(&proc_time_pipe_poll, 1, 0) != 0) {
+            if(-1 == read(proc_time_pipe[READ_SIDE], &proc_time, sizeof(proc_time_t))) {
+                perror("read");
+                exit(-1);
+            }
+            /* Print stats */
         }
     }
     return 0;
 }
 
-/* Takes the string the user inputted and creates an array of the tokens in it */
-char **args_tokenized(char *input_string) {
-    /* Assume no more than 32 space separated tokens was entered */
-    char **args = malloc(sizeof(char**) * 32);
-    char *token;
-    int i;
-
-    /* Ugly hack to be able to know which elements to free */
-    for(i = 0; i < 32; i++) {
-        args[i] = NULL;
-    }
-
-    /* Take one token at a time, allocate and add to 'args' */
-    token = strtok(input_string, " \t");
-    for(i = 0; token != NULL; i++) {
-        args[i] = malloc(strlen(token) + 1);
-        strcpy(args[i], token);
-        token = strtok(NULL, " \t");
-    }
-
-    return args;
-}
-
-/* Frees all tokens in argument and finally the array itself */
-void free_args(char **args) {
-    int i;
-    for(i = 0; args[i] != NULL; i++) {
-       free(args[i]);
-    }
-    free(args);
-}
-
-void exec_background(char **args) {
-    /* TODO Do in background */
+/* Tries to execute the given commands. If is_background is set to TRUE, the
+ * prompt will immediately return. If false, prompt will return when command is
+ * done executing */
+void interpret(char **args, int is_background) {
     int pid; 
+
+    /* TODO Figure out how to separate background/foreground */
+    /* Parent: fork, wait only if foreground
+     *   |    
+     * Child: start time, fork, waitpid, stop time, send proc_time through pipe to parent
+     *   |        
+     * Baby: execute      
+     */
+
     pid = vfork();
-    if(pid == 0) {
-
-        /* in child, execute the command */
-        execvp(args[0], args);
-
-        /* If we get here, execlp returned -1 */
-        perror("exec");
-        exit(-1);
-
-    } else {
-        waitpid(pid, NULL, 0);
-        free_args(args);
-    }
-
-}
-
-void exec_foreground(char **args) {
-    int pid; 
-    pid = vfork();
-    if(pid == 0) {
+    if(0 == pid) {
 
         /* in child, execute the command */
         execvp(args[0], args);
