2de489d5249454753396f9fba531f37ffb5669ff
Isac Arnekvist <isacar@kth.se>
2015-04-13 11:36:04 +0200

    Ludvig fixed segm. error

--- minishell.c
+++ minishell.c
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <poll.h>
 #include "helpers.h"
+#include "proc_clock.h"
 
 #define CMD_MAX_LEN 80
 #define TRUE 1
@@ -16,12 +17,18 @@
 #define READ_SIDE 0
 #define WRITE_SIDE 1
 
+#define SIGDET 1
 /* If SIGDET = 1, then program will listen for signals from child processes
  * to determine that they finished running.
  * If SIGDET = 0, program will use waitpid */
 #ifndef SIGDET
 #define SIGDET 0
 #endif
+
+/* Signal handler for SIGCHLD */
+void child_listener(int sig);
+/* Catches SIGINT */
+void sig_handler(int sig);
 
 /* TODO Bug: run 'cat', terminate with Ctrl-C. This will also terminate minishell. Sighandler? */
 
@@ -37,7 +44,7 @@ int proc_time_pipe[2];
 
 void interpret(char **, int);
 
-/* Sets everything up, loops for checking input and printing stats */
+/* Sets everything up, loop for checking input and printing stats */
 int main() {
     char *input_string;
     char **args;
@@ -56,6 +63,17 @@ int main() {
         exit(-1);
     }
 
+    /* Set up signal handling */
+    if(SIGDET) {
+        if(SIG_ERR == signal(SIGCHLD, child_listener)) {
+            perror("signal");
+            exit(-1);
+        }
+    }
+    if(SIG_ERR == signal(SIGINT, sig_handler)) {
+        perror("signal");
+    }
+
     /* Set up polling of proc_time_pipe */
     proc_time.pid = 0;
     proc_time.delta_millis = 0;
@@ -68,6 +86,7 @@ int main() {
     while(1) {
         /* Read input */
         printf("> ");
+        input_string = NULL;
         read_length = getline(&input_string, &linecap, stdin);
         input_string[read_length - 1] = '\0';
         switch (read_length) {
@@ -96,7 +115,7 @@ int main() {
 
         /* 1: Check if any process sent stats over pipe after they terminated 
          * 2: If it was backgrond, clean up process table */
-        while(poll(&time_poll, 1, 0)) { /* maybe &time_poll is wrong? */
+        while(poll(&time_poll, 1, 0)) { 
             /* This line has problems when compiled on u-shell.csc.kth.se.
              * Gives segmentation error even when inside of loop is not run */
             if(-1 == read(proc_time_pipe[READ_SIDE], &proc_time, sizeof(proc_time))) {
@@ -122,8 +141,8 @@ int main() {
  * done executing */
 void interpret(char **args, int is_background) {
     int pid; 
-    struct timeval start;
-    struct timeval stop;
+    struct timeval start_time;
+    struct timeval stop_time;
     proc_time_t proc_time;
     char *home;
 
@@ -141,23 +160,17 @@ void interpret(char **args, int is_background) {
 
         printf("Goodbye!\n");
         /* TODO Keep track of started processes and kill any non-terminated
-         * processes before quitting */
+         * processes before quitting? */
         exit(0);
 
     } else {
-        /* ## Try to execute given command ##
-         * Parent: fork, wait only if foreground
-         *   |    
-         * Child: start time, fork, waitpid, stop time, send proc_time through pipe to parent
-         *   |        
-         * Baby: execute      
-         */
-        pid = fork();
-        if(0 == pid) {
+        if(SIGDET) {
+            /* Do not wait for background processes, let child_listener do this */
             pid = vfork();
             if(0 == pid) {
 
                 /* in child, execute the command */
+                start(getpid());
                 execvp(args[0], args);
 
                 /* If we get here, execlp returned -1 */
@@ -165,36 +178,104 @@ void interpret(char **args, int is_background) {
                 exit(-1);
 
             } else {
-                /* In parent of executing process */
-                /* Start timer */
-                gettimeofday(&start, NULL);
-                waitpid(pid, NULL, 0);
-                /* Stop timer */
-                gettimeofday(&stop, NULL);
-                
-                /* Prepare stats to send */
-                proc_time.pid = pid;
-                proc_time.delta_millis = (stop.tv_sec - start.tv_sec)*1000 +
-                                         (stop.tv_usec - start.tv_usec)/1000;
-                proc_time.was_background = is_background;
-
-                /* Send */
-                close(proc_time_pipe[READ_SIDE]);
-                write(proc_time_pipe[WRITE_SIDE], &proc_time, sizeof(proc_time));
-                close(proc_time_pipe[WRITE_SIDE]);
-
-                /* The array of arguments was allocated and must be freed */
-                free_args(args);
-                exit(0);
+                /* In parent */
+                if(!is_background) {
+                    waitpid(pid, NULL, 0);
+                }
             }
+
         } else {
-            /* If background process, process table is cleared in main()
-             * when that process finishes */
-            if(!is_background) {
-                waitpid(pid, NULL, 0);
+            /* ## Try to execute given command ##
+             * Since SIGDET = 0, this 'version' waits for commands to finish executing
+             * Parent: fork, wait only if foreground
+             *   |    
+             * Child: start time, fork, waitpid, stop time, send proc_time through pipe to parent
+             *   |        
+             * Baby: execute      
+             */
+            pid = fork();
+            if(0 == pid) {
+                pid = vfork();
+                if(0 == pid) {
+
+                    /* in child, execute the command */
+                    execvp(args[0], args);
+
+                    /* If we get here, execlp returned -1 */
+                    perror("exec");
+                    exit(-1);
+
+                } else {
+                    /* In parent of executing process */
+                    /* Start timer */
+                    gettimeofday(&start_time, NULL);
+                    /* This wait should not be done if SIGDET = 1 */
+                    waitpid(pid, NULL, 0);
+                    /* Stop timer */
+                    gettimeofday(&stop_time, NULL);
+                    
+                    /* Prepare stats to send */
+                    proc_time.pid = pid;
+                    proc_time.delta_millis = (stop_time.tv_sec - start_time.tv_sec)*1000 +
+                                             (stop_time.tv_usec - start_time.tv_usec)/1000;
+                    proc_time.was_background = is_background;
+
+                    /* Send */
+                    close(proc_time_pipe[READ_SIDE]);
+                    write(proc_time_pipe[WRITE_SIDE], &proc_time, sizeof(proc_time));
+                    close(proc_time_pipe[WRITE_SIDE]);
+
+                    /* The array of arguments was allocated and must be freed */
+                    free_args(args);
+                    exit(0);
+                }
             } else {
-                printf("Process %d started in background\n", pid);
+                /* If background process, process table is cleared in main()
+                 * when that process finishes */
+                if(!is_background) {
+                    /* Wait for process if foreground */
+                    waitpid(pid, NULL, 0);
+                } else {
+                    printf("Process %d started in background\n", pid);
+                }
             }
         }
+    }
+}
+
+/* Signal handler for SIGCHLD. Checks if the signal was due to a terminated
+ * process. If it was, acknowledge entry in process tabel via wait() */
+void child_listener(int sig) {
+    pid_t pid;
+    int status, delta;
+    proc_time_t proc_time;
+
+    /* See what process signaled */
+    pid = wait(&status);
+    /* TODO Check why it signaled
+     * TODO Print stats */
+    if(WIFEXITED(status)) {
+        /* Prepare stats to send */
+        delta = stop(pid);
+        proc_time.pid = pid;
+        proc_time.delta_millis = delta;
+        /* The reason for FALSE is that it is no longer a zombie process */
+        proc_time.was_background = FALSE;
+
+        /* Report! */
+        if(0 == write(proc_time_pipe[WRITE_SIDE], &proc_time, sizeof(proc_time))) {
+            perror("write");
+            exit(-1);
+        }
+    } else {
+        /* Nothing to do, maybe next time! */
+    }
+}
+
+/* Makes sure Ctrl-C in child process does not kill miniShell
+ * at the moment also ignores if it happens in 'prompt' mode */
+void sig_handler(int sig) {
+    if(SIGINT == sig) {
+        /* Do nothing, possible to know if sent during child process? */
     }
 }
