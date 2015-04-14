0040f55a5548704009cbefc97c3b5ec6c65135e0
Isac Arnekvist <isacar@kth.se>
2015-04-14 10:31:47 +0200

    minor add

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
-# -O3 skall vara 04 enligt peket
-rm minishell
-gcc -pedantic -Wall -ansi -O3 -o minishell minishell.c helpers.c
+rm -f minishell
+gcc -pedantic -Wall -ansi -O4 -o minishell minishell.c helpers.c proc_clock.c
 ./minishell
