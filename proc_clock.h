53a94ecf1caaf0eceeb5d586fba93828778660f0
Isac Arnekvist <isacar@kth.se>
2015-05-07 14:59:09 +0200

    removed unnecessary files

--- proc_clock.h
+++
@@ -1,9 +0,0 @@
-#ifndef _PROC_CLOCK_H
-#define _PROC_CLOCK_H
-
-/* Starts a timer for the given pid */
-void start(pid_t pid);
-/* Stops the timer for the given pid and returns time in milliseconds */
-int stop(pid_t pid);
-
-#endif
